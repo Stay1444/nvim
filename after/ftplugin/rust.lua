598c653c171bd7726d5eed3a776a079e03df73c3
Stay1444 <quiquelluc@gmail.com>
2023-11-22 17:33:38 +0100

    some changes

---
+++ after/ftplugin/rust.lua
@@ -0,0 +1,8 @@
+local colorscheme = "gruvbox"
+
+vim.defer_fn(function()
+  local ok, err = pcall(vim.api.nvim_command, "colorscheme " .. colorscheme)
+  if not ok then
+    print("Error setting colorscheme: " .. err)
+  end
+end, 1000)
