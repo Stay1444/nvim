28b3590c037b176fbe055bf437a0b3bf2e5490b7
Stay1444 <quiquelluc@gmail.com>
2023-12-23 19:24:38 +0100

    trying to get typescript formatting to play nice

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,8 +1,12 @@
 local colorscheme = "tokyonight"
 
+vim.bo.tabstop = 4 -- size of a hard tabstop (ts).
+vim.bo.shiftwidth = 4 -- size of an indentation (sw).
+vim.bo.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is
+
 vim.defer_fn(function()
-  local ok, err = pcall(vim.api.nvim_command, "colorscheme " .. colorscheme)
-  if not ok then
-    print("Error setting colorscheme: " .. err)
-  end
+    local ok, err = pcall(vim.api.nvim_command, "colorscheme " .. colorscheme)
+    if not ok then
+        print("Error setting colorscheme: " .. err)
+    end
 end, 1000)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,14 @@
 -- Add any additional options here
 local opt = vim.opt
 opt.clipboard = "unnamedplus"
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.softtabstop = 0
+        vim.opt_local.expandtab = true
+        vim.opt_local.smarttab = true
+    end,
+})
